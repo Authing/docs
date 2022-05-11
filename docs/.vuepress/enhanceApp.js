276266cb308d56d3f29c27219bf5f5bf95f1d43c
lixpng <l1968108962@gmail.com>
2022-05-11 15:15:36 +0800

    fix: 提交反馈

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,27 +1,29 @@
-import VueNotification from 'vue-notification/dist/ssr.js';
-import VueSelect from 'vue-select';
-import 'vue-select/src/scss/vue-select.scss';
-import Pagination from 'vue-pagination-2';
+import VueNotification from "vue-notification/dist/ssr.js";
+import VueSelect from "vue-select";
+import "vue-select/src/scss/vue-select.scss";
+import Pagination from "vue-pagination-2";
+import VTooltip from "v-tooltip";
 
 export default ({ Vue, options, router, siteData }) => {
-	Vue.use(VueNotification);
-	Vue.component('v-select', VueSelect);
-	Vue.component('v-pagination', Pagination);
+  Vue.use(VueNotification);
+  Vue.use(VTooltip);
+  Vue.component("v-select", VueSelect);
+  Vue.component("v-pagination", Pagination);
 
-	if (typeof process === 'undefined' || process.env.VUE_ENV !== 'server') {
-		router.onReady(() => {
-			const { app } = router;
+  if (typeof process === "undefined" || process.env.VUE_ENV !== "server") {
+    router.onReady(() => {
+      const { app } = router;
 
-			app.$once('hook:mounted', () => {
-				setTimeout(() => {
-					const { hash } = document.location;
-					if (hash.length > 1) {
-						const id = decodeURIComponent(hash.substring(1));
-						const element = document.getElementById(id);
-						if (element) element.scrollIntoView();
-					}
-				}, 500);
-			});
-		});
-	}
+      app.$once("hook:mounted", () => {
+        setTimeout(() => {
+          const { hash } = document.location;
+          if (hash.length > 1) {
+            const id = decodeURIComponent(hash.substring(1));
+            const element = document.getElementById(id);
+            if (element) element.scrollIntoView();
+          }
+        }, 500);
+      });
+    });
+  }
 };
--- docs/.vuepress/theme/components/Feedback.vue
+++ docs/.vuepress/theme/components/Feedback.vue
@@ -14,73 +14,137 @@
         </a>
       </div>
     </div>
-    <button
-      @click="handleFeedback(STATUS.GOOD)"
-      :class="[
-        'feedback-btn',
-        'good',
-        {
-          active: status === STATUS.GOOD
-        }
-      ]"
-    >
-      <!-- <IconFont v-if="status === STATUS.GOOD" type="authing-good-" /> -->
-      <IconFont type="authing-good-" />
-      {{ feedbackConfig.useful }}
-    </button>
-    <button
-      @click="handleFeedback(STATUS.BAD)"
-      :class="[
-        'feedback-btn',
-        'bad',
-        {
-          active: status === STATUS.BAD
-        }
-      ]"
-    >
-      <!-- <IconFont v-if="status === STATUS.BAD" type="authing-good-" /> -->
-      <IconFont type="authing-good-" />
-      {{ feedbackConfig.useless }}
-    </button>
+    <v-popover placement="bottom-end">
+      <button
+        @click="handleFeedback(STATUS.GOOD)"
+        :class="[
+          'feedback-btn',
+          'good',
+          {
+            active: status === STATUS.GOOD
+          }
+        ]"
+      >
+        <!-- <IconFont v-if="status === STATUS.GOOD" type="authing-good-" /> -->
+        <IconFont type="authing-good-" />
+        {{ feedbackConfig.useful }}
+      </button>
 
-    <div style="position:relative">
-      <div v-if="submited" class="feedback-success">
-        <!-- <div class="feedback-success"> -->
-        <div>
-          图片占位
+      <!-- This will be the content of the popover -->
+      <template slot="popover">
+        <div style="position:relative">
+          <div v-if="submitted" class="feedback-success">
+            <!-- <div class="feedback-success"> -->
+            <div>
+              <img
+                width="122px"
+                src="~@theme/assets/images/feedback-success.png"
+                alt="Feedback Success"
+              />
+            </div>
+            <div>
+              <IconFont
+                type="authing-tijiaochenggong"
+                class="feedback-success-icon"
+              />
+              {{ feedbackConfig.successTip }}
+            </div>
+          </div>
+
+          <div v-else class="bad-reason">
+            <h4 class="bad-reason-title">
+              <span style="color: red">*</span>
+              {{ feedbackConfig.uselessConfig.title }}
+            </h4>
+
+            <CheckboxGroup
+              v-model="badReasons"
+              :options="feedbackConfig.uselessConfig.reasons"
+            />
+
+            <textarea
+              v-model="customReason"
+              class="authing-custom-feedback"
+              placeholder="请详细描述在文档使用中遇到的问题或改进建议（选填）"
+            />
+
+            <button
+              @click="submitFeedbackWithReason"
+              class="submit-feedback-btn"
+            >
+              提交
+            </button>
+            <p class="feedback-help" v-html="feedbackConfig.help"></p>
+          </div>
         </div>
-        <div>
-          <IconFont
-            type="authing-tijiaochenggong"
-            class="feedback-success-icon"
-          />
-          {{ feedbackConfig.successTip }}
+      </template>
+    </v-popover>
+
+    <v-popover placement="bottom-end">
+      <button
+        @click="handleFeedback(STATUS.BAD)"
+        :class="[
+          'feedback-btn',
+          'bad',
+          {
+            active: status === STATUS.BAD
+          }
+        ]"
+      >
+        <!-- <IconFont v-if="status === STATUS.BAD" type="authing-good-" /> -->
+        <IconFont type="authing-good-" />
+        {{ feedbackConfig.useless }}
+      </button>
+
+      <!-- This will be the content of the popover -->
+      <template slot="popover">
+        <div style="position:relative">
+          <div v-if="submitted" class="feedback-success">
+            <!-- <div class="feedback-success"> -->
+            <div>
+              <img
+                width="122px"
+                src="~@theme/assets/images/feedback-success.png"
+                alt="Feedback Success"
+              />
+            </div>
+            <div>
+              <IconFont
+                type="authing-tijiaochenggong"
+                class="feedback-success-icon"
+              />
+              {{ feedbackConfig.successTip }}
+            </div>
+          </div>
+
+          <div v-else class="bad-reason">
+            <h4 class="bad-reason-title">
+              <span style="color: red">*</span>
+              {{ feedbackConfig.uselessConfig.title }}
+            </h4>
+
+            <CheckboxGroup
+              v-model="badReasons"
+              :options="feedbackConfig.uselessConfig.reasons"
+            />
+
+            <textarea
+              v-model="customReason"
+              class="authing-custom-feedback"
+              placeholder="请详细描述在文档使用中遇到的问题或改进建议（选填）"
+            />
+
+            <button
+              @click="submitFeedbackWithReason"
+              class="submit-feedback-btn"
+            >
+              提交
+            </button>
+            <p class="feedback-help" v-html="feedbackConfig.help"></p>
+          </div>
         </div>
-      </div>
-
-      <div v-if="status === STATUS.BAD && !submited" class="bad-reason">
-        <h4 class="bad-reason-title">
-          <span style="color: red">*</span>
-          {{ feedbackConfig.uselessConfig.title }}
-        </h4>
-
-        <CheckboxGroup
-          v-model="badReasons"
-          :options="feedbackConfig.uselessConfig.reasons"
-        />
-
-        <textarea
-          v-model="customReason"
-          class="authing-custom-feedback"
-          placeholder="请详细描述在文档使用中遇到的问题或改进建议（选填）"
-        />
-
-        <button @click="submitFeedbackWithReason" class="submit-feedback-btn">
-          提交
-        </button>
-        <p class="feedback-help" v-html="feedbackConfig.help"></p>
-      </div>
-    </div>
+      </template>
+    </v-popover>
   </div>
 </template>
 
@@ -104,7 +168,8 @@ export default {
       status: STATUS.NONE,
       badReasons: [],
       customReason: "",
-      submited: false
+      submitted: false,
+      submitDialogVisible: false
     };
   },
   computed: {
@@ -117,33 +182,40 @@ export default {
   },
   methods: {
     submitFeedback(params) {
-      feishuFeedback(params).then(() => {});
-      this.submited = true;
+      feishuFeedback(params).then(() => {
+        this.submitted = true;
+      });
     },
     handleFeedback(status) {
       if (status === this.status) {
         return;
       }
-      this.submited = false;
+      this.submitDialogVisible = true;
+      this.submitted = false;
       this.status = status;
 
-      if (this.status === STATUS.GOOD) {
-        this.submitFeedback({
-          helpful: status === STATUS.GOOD,
-          docTitle: this.$page.title,
-          docUrl: window.location.href,
-          customReason: ""
-        });
-      }
+      // if (this.status === STATUS.GOOD) {
+      //   this.submitFeedback({
+      //     helpful: status === STATUS.GOOD,
+      //     docTitle: this.$page.title,
+      //     docUrl: window.location.href,
+      //     customReason: ""
+      //   });
+      // }
     },
     submitFeedbackWithReason() {
       this.submitFeedback({
-        helpful: status === STATUS.GOOD,
+        helpful: this.status === STATUS.GOOD,
         docTitle: this.$page.title,
         docUrl: window.location.href,
         customReason: this.customReason,
         reasonList: this.badReasons
       });
+    },
+    hideSubmitDialog() {
+      if (this.submitDialogVisible) {
+        this.submitDialogVisible = false;
+      }
     }
   }
 };
@@ -191,81 +263,79 @@ export default {
     // &:not(:last-of-type)
     //   border-bottom-right-radius 0
     //   border-top-right-radius 0
-  .authing-checkbox-item
-    flex-grow 0
-    margin-right 49px
-    width auto
+
   .feedback-title
     font-size 16px
     font-weight 400
     color #1D2129
     line-height 26px
     margin 0
-  .feedback-help
+.feedback-help
+  color #6D7278
+  margin-top 14px
+  margin-bottom 0
+  font-size 12px
+  float: right;
+  clear: both;
+.bad-reason
+  overflow: hidden
+  .authing-checkbox-item
+    margin: 0 49px 9px 0
+    flex-grow 0
+    width auto
+  .authing-checkbox
+    display block
+  .bad-reason-title
     color #6D7278
-    margin-top 14px
-    margin-bottom 0
-    font-size 12px
-    float: right;
-    clear: both;
-  .bad-reason
-    .authing-checkbox-item
-      margin: 0
-      margin-bottom 9px
-    .authing-checkbox
-      display block
-    .bad-reason-title
-      color #6D7278
-      margin 0
-      margin-bottom 17px
-      font-size: 14px
-      font-weight normal
-  .feedback-success,.bad-reason
-    position absolute;
-    z-index: 8888;
-    background: #FFFFFF;
-    border: 1px solid #E5E6EB;
-    box-shadow: 0px 16px 32px -10px rgba(4, 24, 115, 0.1);
-    border-radius: 4px;
-    width: 352px;
-    // background: #F8FAFC;
-    padding 24px
-  .feedback-success-icon
-    color #396AFF
-    margin-right 1em
-    flex-shrink 0
-  .authing-custom-feedback
-    width 320px
-    height 90px
-    background: #F2F3F5;
-    border-radius: 2px;
-    border 1px solid #EEEEEE
-    font-size 14px
-    padding 14px 20px
-    margin-top 25px
-    resize auto
+    margin 0
+    margin-bottom 17px
+    font-size: 14px
+    font-weight normal
+.feedback-success,.bad-reason
+  z-index: 8888;
+  background: #FFFFFF;
+  border: 1px solid #E5E6EB;
+  box-shadow: 0px 16px 32px -10px rgba(4, 24, 115, 0.1);
+  border-radius: 4px;
+  width: 352px;
+  // background: #F8FAFC;
+  padding 24px
+.feedback-success-icon
+  color #396AFF
+  margin-right 1em
+  flex-shrink 0
+.authing-custom-feedback
+  width 320px
+  height 90px
+  background: #F2F3F5;
+  border-radius: 2px;
+  border 1px solid #EEEEEE
+  font-size 14px
+  padding 14px 20px
+  margin-top 25px
+  resize auto
 
-    box-sizing border-box
-    &:focus
-      outline none
-  .submit-feedback-btn
-    float right
-    background-color #396AFF
-    background #396AFF
-    border-radius 4px
-    height 40px
-    line-height 40px
-    width 120px
-    margin-top 24px
-    color #fff
+  box-sizing border-box
+  &:focus
+    outline none
+.submit-feedback-btn
+  float right
+  background-color #396AFF
+  background #396AFF
+  border-radius 4px
+  height 40px
+  line-height 40px
+  width 120px
+  margin-top 24px
+  color #fff
+  outline none
+  border none
+  cursor pointer
+  &:focus
     outline none
     border none
-    cursor pointer
-    &:focus
-      outline none
-      border none
-    &:hover
-      background-color #2e55cc
+  &:hover
+    background-color #2e55cc
 
 @media (max-width: $MQMobile)
   .feedback
