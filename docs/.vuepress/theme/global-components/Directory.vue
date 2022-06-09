a16f2eba9cf809e82d72a567431dc5349cbcfcf2
Lucsun <suntianxiang@authing.cn>
2022-06-09 16:05:32 +0800

    Add Directory

---
+++ docs/.vuepress/theme/global-components/Directory.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="directory">
+    <div
+      v-for="(item, index) in directoryList"
+      class="directory-item"
+      :key="index"
+      @click="
+          () => {
+            $router.push(`/${item.link}`);
+          }
+        "
+    >
+      <div class="header">
+        <img
+          width="24px"
+          height="24px"
+          :alt="item.name"
+          :src="
+              require(`@theme/assets/images/sdk-icons/${
+                item.img
+              }`)
+            "
+        />
+        <span class="span-name">{{ item.name }}</span>
+      </div>
+      <IconFont type="authing-jinru" style="font-size: 12px" class="item-language-icon" />
+    </div>
+  </div>
+</template>
+
+<script>
+import IconFont from "@theme/components/IconFont/index.vue";
+
+export default {
+  name: "Directory",
+  components: {
+    IconFont
+  },
+  computed: {
+    directoryList() {
+      return this.$frontmatter.directoryList;
+    }
+  }
+};
+</script>
+
+<style  lang="stylus">
+.directory {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding: 26px 0;
+  border-radius: 4px;
+  box-shadow 0 4px 22px rgba(159, 159, 175, 0.15)
+
+  .directory-item:first-child {
+    margin-top: 0;
+  }
+
+  .directory-item {
+    padding: 0 36px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 26px;
+    cursor: pointer;
+
+    .header {
+      display: flex;
+      align-items: center;
+
+      img {
+        border: 0px
+      }
+
+      .span-name {
+        margin-left: 16px;
+        font-size: 16px;
+        font-weight: bold;
+        line-height: 9px;
+        color: #333333;
+        opacity: 1;
+      }
+    }
+
+    .language-tag {
+      width: 62px;
+      height: 21px;
+      background: #F0F0F0;
+      opacity: 1;
+      border-radius: 4px;
+      font-size: 12px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-left: 8px;
+    }
+  }
+
+}
+</style>
--- docs/reference/guard/v2/README.md
+++ docs/reference/guard/v2/README.md
@@ -1,3 +1,27 @@
+---
+    directoryList: [
+        {
+            name: 'JavaScript',
+            link: 'reference/guard/v2/native-javascript.html',
+            img: 'javascript@2x.png'
+        },
+        {
+            name: 'React',
+            link: 'reference/guard/v2/react.html',
+            img: 'react@2x.png'
+        },
+        {
+            name: 'Vue',
+            link: 'reference/guard/v2/vue.html',
+            img: 'vue@2x.png'
+        },
+        {
+            name: 'Angular',
+            link: 'reference/guard/v2/angular.html',
+            img: 'angular@2x.png'
+        }
+    ]
+---
 # 接入 Authing Guard
 
 <LastUpdated/>
@@ -8,33 +32,35 @@ Authing Guard 是你「即取即用」的登录组件，根据下面的引导完
 
 首先，根据你的应用所使用的语言，选择你的集成方式：
 
+<Directory />
+
 Authing Guard 是 Authing 身份认证和用户管理能力的入口，在为你的应用接入 Authing Guard 后，你的应用将能够使用 Authing 开放的各种认证相关能力，包括但不限于：
 
 ## 丰富多样的认证方式
 
-- 账号密码认证
-- 短信验证码认证
-- 连接外部身份源认证（社交账号登录、企业账号登录）
-- 自建 APP 扫码认证
-- 手机号一键登录
-- 单点登录 SSO
-- 多因素认证 MFA 
+- [账号密码认证](/guides/authentication/basic/password/)
+- [短信验证码认证](/guides/authentication/basic/sms/)
+- [连接外部身份源认证](/guides/connections/)（[社交账号登录](/guides/authentication/social/)、[企业账号登录](/guides/connections/enterprise)）
+- [自建 APP 扫码认证](/guides/authentication/qrcode/use-self-build-app/)
+- [手机号一键登录](//guides/oneauth/)
+- [单点登录 SSO](/reference/sdk-for-sso)
+- [多因素认证 MFA](/guides/app/mfa)
 
 ## 所有主流认证协议
 
-- JWT
-- OIDC、OAuth2.0
-- SAML
-- LDAP
+- [JWT](/concepts/jwt-token)
+- [OIDC、OAuth2.0](/concepts/oidc/oidc-overview)
+- [SAML](/concepts/saml/saml-overview)
+- [LDAP](/guides/org/ldap-user-directory/)
 
-## 自定义登录框样式
+## [自定义登录框样式](/guides/authentication/branding/#样式配置)
 
 - 可视化配置界面
 - 自定义加载图标和背景
 - 登录框内部组件布局
 - 自定义 CSS
 
-## 登录相关的辅助能力
+## [登录相关的辅助能力](/guides/authentication/branding/#功能配置)
 
 - 忘记密码时用户可重置密码
 - 登录注册协议
