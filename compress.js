69ccb3fa1a6bebadc99e5064e92225526e4f70eb
Willin Wang <willin@willin.org>
2022-06-13 11:27:43 +0800

    feat: compress pics

--- compress.js
+++ compress.js
@@ -1,18 +1,51 @@
+const cluster = require("cluster");
+const { cpus } = require("os");
 const { execSync } = require("child_process");
 const path = require("path");
 const fs = require("fs");
-execSync(
-  'find . -name "*.png" -type f -size +1M -not -path "./node_modules/*" -not -path "./docs/.vuepress/dist/*" -not -path "./.git/*" > temp.log'
-);
-const files = fs.readFileSync("./temp.log", { encoding: "utf-8" }).split("\n");
 
-files.forEach(file => {
-  if (!file) {
-    return;
+const numCPUs = cpus().length;
+
+if (cluster.isMaster) {
+  execSync(
+    'find . -name "*.png" -type f -size +500kb -not -path "./node_modules/*" -not -path "./docs/.vuepress/dist/*" -not -path "./.git/*" > temp.log'
+  );
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
   }
-  console.log(`Compressing ${file}`);
-  // 需要先安装 https://pngquant.org/ 命令行工具
-  execSync(`pngquant ${file}`, { encoding: "utf-8" });
-  execSync(`rm -rf ${file}`);
-  execSync(`mv ${file.replace(".png", "-fs8.png")} ${file}`);
-});
+} else {
+  const files = fs
+    .readFileSync("./temp.log", { encoding: "utf-8" })
+    .split("\n");
+  function compress(file) {
+    console.log(`Compressing ${file}`);
+    // 需要先安装 https://pngquant.org/ 命令行工具
+    execSync(`pngquant "${file}"`, { encoding: "utf-8" });
+    const checkStats = execSync(`ls -l "${file}" | grep 'rwxr-xr-x'`, {
+      encoding: "utf-8"
+    });
+    execSync(`rm -rf "${file}"`);
+    execSync(`mv "${file.replace(".png", "-fs8.png")}" "${file}"`);
+    if (checkStats.includes("rwxr-xr-x")) {
+      execSync(`chmod 755 "${file}"`);
+    }
+  }
+  files.forEach((file, i) => {
+    if ((i % numCPUs) + 1 !== +cluster.worker.id) {
+      return;
+    }
+    if (
+      !file ||
+      file.endsWith("/demo1-main.png") ||
+      file.endsWith("-fs8.png")
+    ) {
+      return;
+    }
+    try {
+      compress(file);
+    } catch (e) {
+      console.log(`Failed to compress ${file}`);
+      execSync(`rm -rf "${file.replace(".png", "-fs8.png")}"`);
+    }
+  });
+}
