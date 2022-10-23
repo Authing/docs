ee6e74c3414cabc5439efea1441e1fa6fe4eb863
zhaojingjing <zhaojingjing@authing.cn>
2022-10-23 18:46:24 +0800

    feat: 首页 banner 链接替换

--- docs/.vuepress/theme/components/Home/index.vue
+++ docs/.vuepress/theme/components/Home/index.vue
@@ -15,9 +15,9 @@
         "
       >
         <banner-card
-          :title="data.banner.title"
-          :text="data.banner.text"
-          :url="data.banner.url"
+          :title="data.bannerTitle"
+          :text="data.bannerText"
+          :url="data.bannerUrl"
         />
         <h1 class="home-title">
           {{ data.mainTitle }}
